41bcb8f1f4df98eb2fa07d0e5b9a08eb809b70cf
walaazahir4 <walaazahir4@gmail.com>
2026-09-07 01:37:18 +0300

    responsive done

--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, signal } from '@angular/core';
 import { Button } from "../../shared/ui/button/button";
 import { RouterLinkActive, RouterModule } from "@angular/router";
 
 @Component({
   selector: 'scents-navbar',
-  imports: [Button, RouterModule, RouterLinkActive ],
+  imports: [Button, RouterModule ],
   templateUrl: './navbar.html',
 })
-export class Navbar {
+export class Navbar implements AfterViewInit {
+  isMenuOpen = signal(false);
+ activeSection = signal('home');
+
+  setActive(section: string) {
+    this.activeSection.set(section);
+  }
+ ngAfterViewInit() {
+
+    setTimeout(() => {
+
+      const sections = document.querySelectorAll(
+        '#home, #boutique, #occasions, #about, #contact'
+      );
+
+      const observer = new IntersectionObserver(
+        (entries) => {
+
+          entries.forEach((entry) => {
+
+            if (entry.isIntersecting) {
+              this.activeSection.set(entry.target.id);
+            }
+
+          });
+
+        },
+        {
+          root: null,
+          threshold: 0.2,
+          rootMargin: '-100px 0px -50% 0px'
+        }
+      );
+
+      sections.forEach((section) => {
+        observer.observe(section);
+      });
+
+    }, 500);
+  }
+
 
 }
